266d83c1ad668a87744074acd9f895825f71e2c4
IngridAkeida <ingridsanchesp@gmail.com>
2022-10-24 19:24:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const {list, setList} = useState([]);
+  const {input, setInput} = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    // add list
+    setList([...list, newTodo]);
+
+    //clear list
+    setInput("");
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do List</h1>
+      <input 
+        type="text" 
+        value={input} 
+        onChange={
+        (e) => setInput(e.target.value)
+        }
+      /> 
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button>&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
