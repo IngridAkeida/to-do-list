b29071a46faea0af5bdc3c3698db54cae90592d3
IngridAkeida <ingridsanchesp@gmail.com>
2022-10-25 16:08:22 +0200

    update commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import './App.css';
 import TodoList from './components/todolist'
 
 function App() {
 
   return(
-    <>
+   
+    <div className="App">
       <TodoList/>
-    </>
+    </div>
 
-  )
+  );
 }
 
 export default App;
