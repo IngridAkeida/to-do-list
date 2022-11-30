c8a1a348d00acd08f88d1012ef034d2f6a81e373
IngridAkeida <ingridsanchesp@gmail.com>
2022-11-30 18:23:20 +0100

    update commit

--- src/components/styledtodolist.js
+++ src/components/styledtodolist.js
@@ -6,7 +6,7 @@ export const StyledTodoList = styled.ul`
   overflow: hidden;
   padding: 10px 0 20px 0;
   margin: 48px auto 0;
-  width: 300px;
+  width: 80%;
   font-family: Arial, Helvetica, sans-serif;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
@@ -14,14 +14,24 @@ export const StyledTodoList = styled.ul`
 
 export const StyledTodoItem = styled.li`
   display: flex;
+  justify-content: space-between;
+  position: relative;
 `;
 
-export const StyledTodoText = styled.li`
-  display: flex;
+export const StyledTodoText = styled.p`
+ padding: 0 10px;
+ left: 10px;
+ border: 1px red solid;
+ 
 `;
-
 
 export const StyledTodoButton = styled.button`
-  display: flex;
+  padding: 0 10px;
+  margin: 16px 0;
+  width: 20px;
+  height: 20px;
+  right: 10px;
+  border: 1px red solid;
+  position: absolute;
 `;
 
