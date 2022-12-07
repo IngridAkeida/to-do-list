0ce5590f0b8a76ed956c548963aa24a7fa58fdc7
IngridAkeida <ingridsanchesp@gmail.com>
2022-12-07 16:31:46 +0100

    Fix = Align list items with input Item.

--- src/components/Lists/styledTodoList.js
+++ src/components/Lists/styledTodoList.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledTitle = styled.h1`
   font-family: sans-serif;
   color: #58181f;
+  text-shadow: 1px 1px 0px #ff0000;
 `
 
 export const StyledInput = styled.input`
@@ -10,11 +11,8 @@ export const StyledInput = styled.input`
   text-indent: 10px;
   border: none;
   height: 30px;
-  width: 50%;
+  width: 60%;
   border-radius : 5px 0 0 5px;
-  @media (max-width: 600px) {
-      width: 70%;
-    }
 `
 export const StyledAdd = styled.button`
   background-color: #58181f;
@@ -38,9 +36,8 @@ export const StyledTodoList = styled.ul`
   color: #58181f;
   font-weight: 700;
   background-color: #ff8177;
-  margin: 0;
   overflow: hidden;
-  margin: 48px auto 0;
+  margin: 30px auto 0;
   width: 60%;
   font-family: Arial, Helvetica, sans-serif;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
@@ -50,17 +47,16 @@ export const StyledTodoList = styled.ul`
 export const StyledTodoItem = styled.li`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   position: relative;
 `;
 
 export const StyledTodoText = styled.p`
- padding: 0 10px;
- left: 10px; 
+ 
 `;
 
 export const StyledTodoButton = styled.button`
   padding: 0;
-  margin: 16px 0;
   width: 45px;
   height: 25px;
   right: 50px;
